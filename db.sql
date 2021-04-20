b45cc1a11a59d2b09ae3932082c2c40073edc691
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-04-20 01:15:30 -0500

    Agregado archivo de la DB (MySQL)

--- db.sql
+++ db.sql
@@ -4,20 +4,23 @@ use calenbellback_db;
 create table usuarios(
 	id_usuario int primary key auto_increment not null,
 	nombres char(20),
-	correo varchar(255),
-	usuario char(40),
-    id_provisional varchar(50) not null,
+	correo varchar(255) unique not null,
+	usuario char(40) not null unique,
+    id_provisional char(50) unique not null,
+    numero char(10) unique null,
+    otp_token varchar(100) unique null,
 	password varchar(255) not null
 );
 create table eventos(
 	id int primary key auto_increment not null,
 	titulo char(70),
 	hora time,
-	dia date,
+	fecha date,
 	descripcion varchar(250),
-	codigo int not null,
+	codigo char(50) not null,
     tipo_ev char(1) not null,
-    foreign key (codigo) references usuarios(id_usuario)
+    icono varchar(4),
+    foreign key (codigo) references usuarios(id_provisional)
 );
 create table eventos_grupales(
 	id int primary key auto_increment not null,
@@ -33,7 +36,3 @@ create table usuarios_contactos(
     foreign key (idusuario) references usuarios(id_usuario),
     foreign key (id_contacto) references usuarios(id_usuario)
 );
-
-#alter table eventos add FOREIGN KEY (codigo) REFERENCES usuarios(id_usuario);
-
-#INSERT INTO eventos (titulo, hora, dia, descripcion,codigo) VALUES ('momito', '22:37:22', '2020-09-18', 'cumpleaños del momito',1);

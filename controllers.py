f48ffc800d6af9867902918ca0eeeccd6422808d
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-05-10 20:09:53 -0500

    Eliminada importación innecesaria, agregado controlador de recuperación de contraseña, actualizado controlador de eventos [agregado borrado y actualizado de eventos],

--- controllers.py
+++ controllers.py
@@ -1,7 +1,7 @@
 import time
 from flask.views import MethodView
 from flask import jsonify, request
-from services import fixStringClient, decryptStringBcrypt
+from services import fixStringClient
 from services import encoded_jwt
 from model import Models
 
@@ -26,7 +26,7 @@ class LoginUserControllers(MethodView):
         dataDB = Model.login()
         
         return jsonify(dataDB), 200
-        
+
 class RegisterUserControllers(MethodView):
     """
         Register
@@ -52,45 +52,6 @@ class RegisterUserControllers(MethodView):
             data = Model.register()
             
             return jsonify(data), 200
-
-class EventsControllers(MethodView):
-    def get(self):
-        token = request.headers.get('Authorization').split(" ")[1] if request.headers.get('Authorization') else None
-
-        Model = Models()
-        Model.info = {
-            'token': token
-        }
-        data = Model.getEvents()
-
-        time.sleep(1)
-        return jsonify(data), 200
-
-    def post(self):
-        token = request.headers.get('Authorization').split(" ")[1]
-
-        json_request = request.get_json()
-
-        Model = Models()
-        Model.info = {
-            'token': token,
-            'title': fixStringClient(json_request.get("title")),
-            'hour': fixStringClient(json_request.get("hour")),
-            'date': fixStringClient(json_request.get("date")),
-            'description': fixStringClient(json_request.get("description")),
-            'type_ev': fixStringClient(json_request.get("type_ev")),
-            'icon': fixStringClient(json_request.get("icon")),
-        }
-        data = Model.registerEvents()
-
-        time.sleep(5)
-        return jsonify(data), 200
-
-    def put(self):
-        pass
-
-    def delete(self):
-        pass
 
 class RecoveryAccountControllers(MethodView):
     def post(self):
@@ -136,4 +97,77 @@ class validateChangePwdControllers(MethodView):
         dataDB = Model.validateChangePwd()
         
         return jsonify(dataDB), 200
+
+class EventsControllers(MethodView):
+    def get(self):
+        token = request.headers.get('Authorization').split(" ")[1] if request.headers.get('Authorization') else None
+
+        Model = Models()
+        Model.info = {
+            'token': token
+        }
+        data = Model.getEvents()
+
+        # time.sleep(1)
+        return jsonify(data), 200
+
+    def post(self):
+        token = request.headers.get('Authorization').split(" ")[1] if request.headers.get('Authorization') else None
+
+        json_request = request.get_json()
+        
+
+        Model = Models()
+        Model.info = {
+            'token': token,
+            'title': fixStringClient(json_request.get("title")),
+            'hour': fixStringClient(json_request.get("hour")),
+            'date': fixStringClient(json_request.get("date")),
+            'description': fixStringClient(json_request.get("description")),
+            'type_ev': fixStringClient(json_request.get("type_ev")),
+            'icon': fixStringClient(json_request.get("icon")),
+        }
+        data = Model.registerEvents()
+
+        # time.sleep(5)
+        return jsonify(data), 200
+
+    def put(self):
+        token = request.headers.get('Authorization').split(" ")[1] if request.headers.get('Authorization') else None
+
+        json_request = request.get_json()
+        
+
+        Model = Models()
+        Model.info = {
+            'token': token,
+            'title': fixStringClient(json_request.get("title")),
+            'hour': fixStringClient(json_request.get("hour")),
+            'date': fixStringClient(json_request.get("date")),
+            'description': fixStringClient(json_request.get("description")),
+            'type_ev': fixStringClient(json_request.get("type_ev")),
+            'icon': fixStringClient(json_request.get("icon")),
+            'id_event': fixStringClient(json_request.get("id_event")),
+        }
+        
+        data = Model.changeEvents()
+
+        # time.sleep(5)
+        return jsonify(data), 200
+
+    def delete(self):
+        token = request.headers.get('Authorization').split(" ")[1] if request.headers.get('Authorization') else None
+
+        events = request.headers.get('Body').split(" ")[1] if request.headers.get('Body') else None
+        
+        Model = Models()
+        Model.info = {
+            'token': fixStringClient(token),
+            'events': fixStringClient(events)
+        }
+
+        data = Model.deleteEvents()
+
+        # time.sleep(5)
+        return jsonify(data), 200
 

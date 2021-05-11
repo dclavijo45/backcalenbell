5f79c8d9df68f1583aad06d2548d6f41e2516592
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-05-10 20:21:36 -0500

    Cambiado configuración del servidor por medio de acceso a variables de entorno

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import varenviron
 from __init__ import app
+from os import environ as env
 
-PORT = 5000
-DEBUG = True
+PORT = env['PORT'] if env['PORT'] else 3000
+DEBUG = True if env['DEBUG'] == '1' else False
+HOST = env['HOST'] if env['HOST'] else 'localhost'
 
 app.run(port=PORT, debug=DEBUG)

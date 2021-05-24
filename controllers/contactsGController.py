a230e3923d4bdbe134fdc7f7ed4a2ad9a74e3389
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-05-24 04:41:32 -0500

    Agregado sistema para agregar y eliminar contactos

--- controllers/contactsGController.py
+++ controllers/contactsGController.py
@@ -18,10 +18,31 @@ class ContactsGControllers(MethodView):
         return jsonify(data), 200
     
     def post(self):
-        pass
+        token = request.headers.get('Authorization').split(" ")[1] if request.headers.get('Authorization') else None
 
-    def put(self):
-        pass
+        json_request = request.get_json(force=True)
+
+        Model = Models()
+        Model.info = {
+            'token': token,
+            'user_add': fixStringClient(json_request.get("user_add"))
+        }
+        data = Model.addContacts()
+
+        # time.sleep(1)
+        return jsonify(data), 200
 
     def delete(self):
-        pass
+        token = request.headers.get('Authorization').split(" ")[1] if request.headers.get('Authorization') else None
+
+        payload = request.headers.get('Body').split(" ")[1] if request.headers.get('Body') else None # pos: 1 = id_contactG, 2 = type contact
+
+        Model = Models()
+        Model.info = {
+            'token': token,
+            'payload': fixStringClient(payload)
+        }
+        data = Model.deleteContacts()
+
+        # time.sleep(1)
+        return jsonify(data), 200

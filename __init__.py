e7604ae71f615d07e525fa60724e2ea9497680d9
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-06-04 23:57:35 -0500

    Agregado reglas de rutas para el control de manejo de eventos y solicitudes de amistad

--- __init__.py
+++ __init__.py
@@ -7,14 +7,17 @@ app = Flask(__name__)
 
 app.secret_key = SECRET_KEY
 
-CORS(app, resources={
-    r"/*": {'origins': '*'},
-    r"/*": {
-        'origins': ['*'],
-        'methods': ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'],
-        'allow_headers': ['Authorization', 'Content-Type', 'Body'],
-        }
-    })
+CORS(
+    app,
+    resources={
+        r"/*": {"origins": "*"},
+        r"/*": {
+            "origins": ["*"],
+            "methods": ["OPTIONS", "GET", "POST", "PUT", "DELETE"],
+            "allow_headers": ["Authorization", "Content-Type", "Body"],
+        },
+    },
+)
 
 app.add_url_rule(user["login_user"], view_func=user["login_user_controllers"])
 
@@ -22,17 +25,38 @@ app.add_url_rule(user["register_user"], view_func=user["register_user_controller
 
 app.add_url_rule(logged["manage_events"], view_func=logged["manage_events_controllers"])
 
-app.add_url_rule(user["recovery_account"], view_func=user["recovery_account_controllers"])
+app.add_url_rule(
+    user["recovery_account"], view_func=user["recovery_account_controllers"]
+)
 
-app.add_url_rule(user["validate_code_recovery_account"], view_func=user["validate_code_recovery_account_controllers"])
+app.add_url_rule(
+    user["validate_code_recovery_account"],
+    view_func=user["validate_code_recovery_account_controllers"],
+)
 
 app.add_url_rule(user["change_pwd"], view_func=user["change_pwd_controllers"])
 
 app.add_url_rule(logged["init_chat"], view_func=logged["init_chat_controllers"])
 
-app.add_url_rule(logged["manage_contactsG"], view_func=logged["manage_contactsG_controllers"])
+app.add_url_rule(
+    logged["manage_contactsG"], view_func=logged["manage_contactsG_controllers"]
+)
 
-app.add_url_rule(logged["search_contacts"], view_func=logged["search_contacts_controllers"])
+app.add_url_rule(
+    logged["search_contacts"], view_func=logged["search_contacts_controllers"]
+)
 
-app.add_url_rule(logged["manage_query_friends"], view_func=logged["manage_query_friends_controllers"])
+app.add_url_rule(
+    logged["manage_query_friends"], view_func=logged["manage_query_friends_controllers"]
+)
 
+app.add_url_rule(
+    logged["participants_event"], view_func=logged["participants_event_controllers"]
+)
+
+app.add_url_rule(logged["join_event"], view_func=logged["join_event_controllers"])
+
+app.add_url_rule(
+    logged["delete_participant_event"],
+    view_func=logged["delete_participant_event_controllers"],
+)

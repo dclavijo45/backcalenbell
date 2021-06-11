b0331b20bede223198d09673a188118bd91c04f9
Daniel Clavijo Gonzalez <danielclavijo19380@gmail.com>
2021-06-10 23:57:17 -0500

    Update: Se ha actualizado el logo en las plantillas de correo

--- utils/utilTemplate.py
+++ utils/utilTemplate.py
@@ -1,12 +1,12 @@
 from config.config import SERVER_BACK
 
-logoIMG = "https://www.dropbox.com/s/du22pgt8wnw0yob/ProductLogo.png?dl=1"
+logoIMG = "https://www.dropbox.com/s/vonrx4kb0guddk1/logo-email.png?dl=1"
 
 
 class UTemplates:
     def emailHtmlDefault(self):
         return (
-            """<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"><table style="width:645px;font-family:arial,sans-serif,tahoma;margin:0px;background:#474747"><tbody><tr><td colspan="4" style="border-bottom: 2px solid #474747;padding:10px;background:#fff"><a href="#" target="_blank" style="display: inline;text-decoration-line: none;"><img src="https://www.dropbox.com/s/du22pgt8wnw0yob/ProductLogo.png?dl=1" style="height: 93px;width: 90px;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px"><font size="2"><div>Estimado(a) <a style="color:#000022;">"""
+            """<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"><table style="width:645px;font-family:arial,sans-serif,tahoma;margin:0px;background:#474747"><tbody><tr><td colspan="4" style="border-bottom: 2px solid #474747;padding:10px;background:#fff"><a href="#" target="_blank" style="display: inline;text-decoration-line: none;"><img src="https://www.dropbox.com/s/du22pgt8wnw0yob/ProductLogo.png?dl=1" style="height: 93px;width: 100%;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px"><font size="2"><div>Estimado(a) <a style="color:#000022;">"""
             + self.info["name"]
             + """</a>,<br><p>Bienvenido a Calenbell</p><p>Nos alegra que seas parte de nosotros!, de ahoraen adelante tendrás disponible una agenda persolal que te seguirá todo el tiempo mientras estés en linea, si no hiciste este registro contáctanos, nosotros nos encargamos del resto :) , en Calenbell Estamos para lo que necesites</b></p></div>Atentamente<br><br>Equipo de Calenbell</font></td></tr></tbody></table>"""
         )
@@ -15,7 +15,7 @@ class UTemplates:
         return (
             """<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"><table style="width:645px;font-family:arial,sans-serif,tahoma;margin:0px;background:#474747"><tbody><tr><td colspan="4" style="border-bottom: 2px solid #474747;padding:10px;background:#fff"><a href="#" target="_blank" style="display: inline;text-decoration-line: none;"><img src='"""
             + logoIMG
-            + """' style="height: 93px;width: 90px;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px"><font size="2"><div>Estimado(a) <a style="color:#000022;">"""
+            + """' style="height: 93px;width: 100%;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px"><font size="2"><div>Estimado(a) <a style="color:#000022;">"""
             + self.info["name"]
             + """</a>,<br><p>Se ha solicitado recuperación de su contraseña</p><p>Nos alegra que seas parte de nosotros!, se acaba de solicitar reestablecimiento de tu contraseña, si no hiciste esta petición solo ignóralo, nosotros nos encargamos del resto :) , en Calenbell Estamos para lo que necesites, solo contáctanos!</b></p></div>Atentamente<br><br>Equipo de Calenbell</font></td></tr></tbody></table>"""
         )
@@ -24,7 +24,7 @@ class UTemplates:
         return (
             """<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"><table style="width:645px;font-family:arial,sans-serif,tahoma;margin:0px;background:#474747"><tbody><tr><td colspan="4" style="border-bottom: 2px solid #474747;padding:10px;background:#fff"><a href="#" target="_blank" style="display: inline;text-decoration-line: none;"><img src='"""
             + logoIMG
-            + """' style="height: 93px;width: 90px;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px"><font size="2"><div>Estimado(a) <a style="color:#000022;">"""
+            + """' style="height: 93px;width: 100%;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px"><font size="2"><div>Estimado(a) <a style="color:#000022;">"""
             + self.info["name"]
             + """</a>,<br><p>Se ha solicitado recuperación de su contraseña</p><p>Código de recuperación: <b>"""
             + self.info["code"]
@@ -35,7 +35,7 @@ class UTemplates:
         return (
             """<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"><table style="width:645px;font-family:arial,sans-serif,tahoma;margin:0px;background:#474747"><tbody><tr><td colspan="4" style="border-bottom: 2px solid #474747;padding:10px;background:#fff"><a href="#" target="_blank" style="display: inline;text-decoration-line: none;"><img src='"""
             + logoIMG
-            + """' style="height: 93px;width: 90px;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px; font-size: 15px;"><div>Estimado(a) <a style="color:#000022;">"""
+            + """' style="height: 93px;width: 100%;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px; font-size: 15px;"><div>Estimado(a) <a style="color:#000022;">"""
             + self.info["name"]
             + """</a>,<br><p><img src='"""
             + self.info["photo_contact"]
@@ -56,7 +56,7 @@ class UTemplates:
         return (
             """<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"><table style="width:645px;font-family:arial,sans-serif,tahoma;margin:0px;background:#474747"><tbody><tr><td colspan="4" style="border-bottom: 2px solid #474747;padding:10px;background:#fff"><a href="#" target="_blank" style="display: inline;text-decoration-line: none;"><img src='"""
             + logoIMG
-            + """' style="height: 93px;width: 90px;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px; font-size: 15px;"><div>Estimado(a) <a style="color:#000022;">"""
+            + """' style="height: 93px;width: 100%;border: none;"></a></td></tr><tr><td colspan="4" style="background:#fff;padding:10px; font-size: 15px;"><div>Estimado(a) <a style="color:#000022;">"""
             + self.info["name"]
             + """</a>,<br><p><img src='"""
             + self.info["photo_contact"]
@@ -79,7 +79,7 @@ class UTemplates:
         return (
             """<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"><table style="width:645px;font-family:arial,sans-serif,tahoma;margin:0px;background:#474747"><tbody><tr><td colspan="4" style="border-bottom: 2px solid #474747;padding:10px;background:#fff"><a href="#" target="_blank" style='display: inline;text-decoration-line: none;'><img src='"""
             + logoIMG
-            + """' style='height: 93px;width: 90px;border: none;'></a></td></tr><tr><td colspan='4' style='background:#fff;padding:10px; font-size: 15px;'><div>Estimado(a) <a style='color:#000022;'>"""
+            + """' style='height: 93px;width: 100%;border: none;'></a></td></tr><tr><td colspan='4' style='background:#fff;padding:10px; font-size: 15px;'><div>Estimado(a) <a style='color:#000022;'>"""
             + self.info["name"]
             + """</a>,<br><p><img src='"""
             + self.info["photo_contact"]
